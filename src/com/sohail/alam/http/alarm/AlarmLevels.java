03c110144c55a0f11a36e3993b965bc3696d1443
Sohail Alam <sohailalam2@hotmail.com>
2013-09-25 01:03:14 +0530

    PHP Processing, Refactoring, Optimization

---
+++ src/com/sohail/alam/http/alarm/AlarmLevels.java
@@ -0,0 +1,17 @@
+package com.sohail.alam.http.alarm;
+
+/**
+ * User: Sohail Alam
+ * Version: 1.0.0
+ * Date: 24/9/13
+ * Time: 1:08 AM
+ */
+public interface AlarmLevels {
+    // ITU Perceived Severity      syslog SEVERITY (Name)
+    public static final int CRITICAL = 1; //(Alert)
+    public static final int MAJOR = 2; //(Critical)
+    public static final int MINOR = 3; //(Error)
+    public static final int WARNING = 4; //(Warning)
+    public static final int INDETERMINATE = 5; //(Notice)
+    public static final int CLEARED = 5; //(Notice)
+}
