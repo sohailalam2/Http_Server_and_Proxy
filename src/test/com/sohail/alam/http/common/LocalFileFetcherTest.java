999c585a3d8789559149eb4ae84a68aa9d9bd3f2
Sohail Alam <sohailalam2@hotmail.com>
2013-09-26 22:27:11 +0530

    Updated Copyright

--- src/test/com/sohail/alam/http/common/LocalFileFetcherTest.java
+++ src/test/com/sohail/alam/http/common/LocalFileFetcherTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2013 The Http Server & Proxy
  *
- *  The File Watch Service Project licenses this file to you under the Apache License, version 2.0 (the "License");
+ *  The Http Server & Proxy Project licenses this file to you under the Apache License, version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at:
  *
